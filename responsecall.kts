5c29a51d39f83eb9d0881ab3bde0bb87ec7b3f2a
Javed-Pc <106265348+Javed-Pc@users.noreply.github.com>
2022-09-18 14:48:48 +0530

    Create responsecall.kts

---
+++ responsecall.kts
@@ -0,0 +1,40 @@
+   {
+                "debug_log" : {
+                "line" : []
+                },
+                "html_attributions" : [],
+                "logging_info" : {
+                "experiment_id" : [],
+                "query_geographic_location" : "AU"
+                },
+                "results" : [
+                {
+                    "geometry" : {
+                        "location" : {
+                            "lat" : -33.86879,
+                            "lng" : 151.194217
+                        }
+                    },
+                    "icon" : "http://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+                    "id" : "21a0b251c9b8392186142c798263e289fe45b4aa",
+                    "name" : "Rhythmboat Cruises",
+                    "opening_hours" : {
+                        "open_now" : false,
+                        "weekday_text" : []
+                    },
+                    "photos" : [
+                        {
+                            "height" : 426,
+                            "html_attributions" : [
+                            "\u003ca href=\"https://www.google.com/maps/views/profile/104066891898402903288\"\u003eRhythmboat Cruises\u003c/a\u003e"
+                            ],
+                            "photo_reference" : "CmRdAAAA-YL_I_Gk02blOX6S0nKHry8PFu9pDyp3Y9AnqISsa3Eq8mkbdD5mXuu1Fax60s0nSy3iiX-h5j-ztyLHcc1-782MsLQsgLLa4t3ZgDmCMll-a8ABapZGnZwDKByk67LFEhBgedv_u_eYFsEo9ay8jxJjGhTUHKPJ4G82vBJqSNliuv7UlAtclw",
+                            "width" : 640
+                        }
+                    ],
+                    "place_id" : "ChIJyWEHuEmuEmsRm9hTkapTCrk",
+                    "reference" : "CnRmAAAAvQlMKw-XtxEY4vWFCvudF7CEMQGI5ycNbfVgGl9rAF75fdiPOiLJw1k9NL2v8ZIJsOJuRS3Lm9Dw1vga4ajycAs7PlxN1MVnnYT9la0pBvEvSQNlyvszKANS1R4P7Mvk_jhqswMggqCUtwJ13LN2hRIQOiAkLTWUi3DOjVVOw7J5IRoUb_cJyJaJNqKdmkDM2f0OjQjh9F0",
+                    "scope" : "GOOGLE",
+                    "types" : [ "restaurant", "food", "point_of_interest", "establishment" ],
+                    "vicinity" : "Pyrmont Bay Wharf (Near Australia Maritime Museum), Pyrmont, NSW 2009"
+                },
